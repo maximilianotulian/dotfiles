19907eb83933571017e3bea69dc83a867f771eff
Maximiliano Tulian <maxtulian93@gmail.com>
2017-12-01 20:19:32 -0300

    Update installation script

--- install.sh
+++ install.sh
@@ -1,23 +1,48 @@
 #!/usr/bin/env bash
 
+
 # Get current dir (so run this script from anywhere)
 
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+
 # Make utilities available
 
 PATH="$DOTFILES_DIR/bin:$PATH"
 
+
+# Check for executable install script 
+
+if [ -x "$DOTFILES_DIR/install.sh" ]; then
+	printf "Please give executable permisions to '${DOTFILES_DIR}/install.sh'";
+	exit 0;
+fi
+
+
 # Update dotfiles itself first
 
-if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+if [ -x "$(command -v git)" ]
+then
+	printf "Updating dot files"
+  	git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master;
+fi
 
-# Bunch of symlinks
+# Copy configuration files
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
-# Add editor conf and link to system files
+
+# Directories
+
+BASHRC_FILE=~/.bashrc
+GIT_CONFIG=~/.gitconfig
+OHMYSZH_DIR=~/.oh-my-zsh
+TMUX_FILE=~/.tmux.conf
+TMUX_TPM_DIR=~/.tmux/plugins/tpm/
+VIMRC_FILE=~/.vimrc
+VUNDLE=~/.vim/bundle/Vundle.vim
+ZSH_FILE=~/.zshrc
