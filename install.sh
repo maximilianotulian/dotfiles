fda3599087d4c141b8a3bfa4f2e55a6811c19251
Maximiliano Tulian <m.tulian@globant.com>
2017-12-01 17:44:46 -0300

    Start working in install.sh script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Make utilities available
+
+PATH="$DOTFILES_DIR/bin:$PATH"
+
+# Update dotfiles itself first
+
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+
+# Add editor conf and link to system files
