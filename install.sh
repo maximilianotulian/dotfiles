2898e1fbede39b057d45970fb12b6dab7763022e
Maximiliano Tulian <m.tulian@globant.com>
2018-01-20 21:16:19 -0300

    Disable symbolic link for bash_profile.

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ fi
 
 # Copy configuration files
 
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+#ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
